42bdde62b0ce4f148c5a65459476f59039dd6eb7
nemo-omen <nemo.omen@gmail.com>
2021-11-30 19:13:18 -0600

    Challenge exercises

--- Playground/src/App.java
+++ Playground/src/App.java
@@ -1,46 +1,17 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class App {
-
-    public static double sum (ArrayList<Double> list) {
-        double sum = 0;
-
-        for(int i = 0; i < list.size(); i++) {
-            sum += list.get(i);
-        }
-
-        return sum;
-    }
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        ArrayList<Double> list = new ArrayList<Double>();
-
-        System.out.println("This program will store numbers in an ArrayList and compute the sum of numbers.");
-        System.out.println("The numbers will be read in one at a time. A 0 will terminate the input.");
-        
-        double newDouble = input.nextDouble();
-
-        while(newDouble != 0.0) {
-            list.add(newDouble);
-            newDouble = input.nextDouble();
-        }
-
-        String alistOutput = "The items in the ArrayList are [";
-        
-        for(int i = 0; i < list.size(); i++) {
-            alistOutput += list.get(i).toString();
-            if(i == list.size() - 1) {
-                alistOutput += "]";
-            } else {
-                alistOutput += ", ";
-            }
-        }
-
-        System.out.println(alistOutput);
-        
-
-        System.out.printf("There are %d items in the ArrayList%n", list.size());
-        System.out.printf("The sum of items in the ArrayList is %.1f%n", sum(list));
-    }
+        String name1;
+        String name2;
+  
+        name1 = "Sam";
+        name2 = "Lucy";
+  
+        System.out.printf("%-8s/\n", name1);
+        System.out.printf("%-8s/\n", name2);
+     }
 }
